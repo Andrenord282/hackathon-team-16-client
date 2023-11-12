18765af9f35c7f69afd33837d67b874a31511d89
Stohelitskii <Andrenord282@gmail.com>
2023-11-12 12:48:09 +0500

    улучшил работу имя с тегом

--- src/widgets/GameEntry/hooks/useGameEntryState.js
+++ src/widgets/GameEntry/hooks/useGameEntryState.js
@@ -24,12 +24,25 @@ const useGameEntryState = () => {
     }, [player]);
 
     useEffect(() => {
-        if (userNameInput.length > 5 && /#\d\d\d\d$/.test(userNameInput)) {
-            const cursor = userNameInput.length - 5;
-            userNameInputRef.current.selectionStart = userNameInputRef.current.selectionEnd = cursor;
-        } else {
-            const cursor = userNameInput.length;
-            userNameInputRef.current.selectionStart = userNameInputRef.current.selectionEnd = cursor;
+        let cursor = 0;
+        switch (true) {
+            case userNameInput.length > 5 && /#\d\d\d\d$/.test(userNameInput):
+                cursor = userNameInput.length - 5;
+                userNameInputRef.current.selectionStart = userNameInputRef.current.selectionEnd = cursor;
+                break;
+            case userNameInput.length > 5 && !/#\d\d\d\d$/.test(userNameInput):
+                cursor = userNameInput.length;
+                userNameInputRef.current.selectionStart = userNameInputRef.current.selectionEnd = cursor;
+                break;
+            case userNameInput.length === 5 && /#\d\d\d\d$/.test(userNameInput):
+                cursor = 0;
+                userNameInputRef.current.selectionStart = userNameInputRef.current.selectionEnd = cursor;
+                setUserNameInput("");
+                handlerSetPlayer("");
+                break;
+
+            default:
+                break;
         }
     }, [userNameInput]);
 
