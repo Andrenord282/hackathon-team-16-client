6231a51bb602d0b3abd6278f6d58f748be5ebda2
Stohelitskii <Andrenord282@gmail.com>
2023-11-12 17:24:11 +0500

    внес правки

--- src/widgets/GameBoard/components/GameStatistics/GameStatistics.jsx
+++ src/widgets/GameBoard/components/GameStatistics/GameStatistics.jsx
@@ -2,15 +2,29 @@ import classNames from "classnames";
 import { useSetRenderTimeType } from "./hooks/useSetRenderTimeType";
 
 import "./GameStatistics.scss";
+import { useEffect, useState } from "react";
 
 const GameStatistics = (props) => {
     const { className, player, movesCount, elapsedTimer, countdownTimer, gameСountdownTimer } = props;
+    const [playerDisplay, setPlayerDisplay] = useState([]);
     const { setRenderTimeType } = useSetRenderTimeType();
+
+    useEffect(() => {
+        const splitIndex = player.indexOf("#");
+
+        const name = player.substring(0, splitIndex);
+        const tag = player.substring(splitIndex);
+
+        setPlayerDisplay([name, tag]);
+    }, [player]);
 
     return (
         <div className={`game-statistics ${classNames(className)}`}>
             <div className="game-statistics__item">
-                <span className="game-statistics__item-value game-statistics__item-value_player">{player}</span>
+                <span className="game-statistics__item-value game-statistics__item-value_player">
+                    {playerDisplay[0]} <br />
+                    {playerDisplay[1]}{" "}
+                </span>
                 <span className="game-statistics__item-text">игрок</span>
             </div>
             <div className="game-statistics__item">
