e6fa3cdff587a0bd44a19664bca0188c087cd4e3
Stohelitskii <Andrenord282@gmail.com>
2023-11-12 15:51:39 +0500

    доделал экран победы

--- src/widgets/GameWin/GameWin.jsx
+++ src/widgets/GameWin/GameWin.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useGameWinState } from "./hooks/useGameWinState";
+import { useEffect } from "react";
+import CatImg from "assets/img/cat.png";
 
 import "./GameWin.scss";
-import { useEffect } from "react";
-
 const GameWin = () => {
     const navigate = useNavigate();
     const { player, elapsedTimer, movesCount } = useGameWinState();
@@ -18,9 +18,20 @@ const GameWin = () => {
     return (
         <div className="game-win">
             <div className="game-win__content">
+                <img src={CatImg} className="game-win__cat-img" alt="" />
                 <h1 className="game-win__title">
-                    {player}, <br /> Поздравляем! <br /> Игра окончена
+                    {player}, Поздравляем! <br /> Ты собрал все карточки
                 </h1>
+                <div className="game-win__descr">
+                    <p className="game-win__descr-text">
+                        Узнать о проблеме — первый шаг к реальным переменам. Теперь, когда ты узнал больше о сиротстве,
+                        давайте превратим знание{" "}
+                        <a href="https://docs.google.com/document/d/1ilDuHz-TFXP_ID6eBZvFRqnb8emcTq2Jy1KeA5S2z_g/edit">
+                            в действие
+                        </a>
+                        .
+                    </p>
+                </div>
                 <div className="game-win__statistics">
                     <div className="game-win__statistics-item">
                         <span className="game-win__statistics-item-value">{elapsedTimer}</span>
